a1395d9a64abb41ccc670f1a099d256e5a03165e
wisemenofgod <3030935303@qq.com>
2021-11-01 15:12:52 +0800

    1.0 version

---
+++ src/main/java/com/wisesmenofgod/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.wisesmenofgod.git;
+
+/*
+    com.wisemenofgod
+    2021-11-01-14:34
+*/public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("时间总会说出再见");
+    }
+}
