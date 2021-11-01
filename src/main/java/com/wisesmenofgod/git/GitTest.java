a29ef6ea599d680502ef1eab470110e6ea2f414d
wisemenofgod <3030935303@qq.com>
2021-11-01 15:18:06 +0800

    1.2 version

--- src/main/java/com/wisesmenofgod/git/GitTest.java
+++ src/main/java/com/wisesmenofgod/git/GitTest.java
@@ -7,5 +7,6 @@ package com.wisesmenofgod.git;
     public static void main(String[] args) {
         System.out.println("时间总会说出再见");
         System.out.println("海鸟跟鱼相爱,只是一场意外");
+        System.out.println("爱上着满珊瑚海");
     }
 }
